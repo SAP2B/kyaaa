d7a084ed6d3261c9da9f595a35d02fd2960ef968
SAP2B <SAP2B@users.noreply.github.com>
2026-09-07 17:57:05 +0000

    deploy: 485e24010e30c7c848166d00a25b1406bca0ff26

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b7ef78ec.js',
+                path_to_root + 'searchindex-4b4e9b4b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
